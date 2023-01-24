24c6dc78edb4091d849b1372094d5f1ab02d5226
elcomandamahmoud <elcomandamahmoud2011@gmail.com>
2023-01-24 18:06:24 +0200

    Add project files.

---
+++ Asignment13/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Asignment13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LBLName = new System.Windows.Forms.Label();
+            this.TXTName = new System.Windows.Forms.TextBox();
+            this.LBLAge = new System.Windows.Forms.Label();
+            this.TXTAge = new System.Windows.Forms.TextBox();
+            this.BTNWelcome = new System.Windows.Forms.Button();
+            this.BTNExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LBLName
+            // 
+            resources.ApplyResources(this.LBLName, "LBLName");
+            this.LBLName.Name = "LBLName";
+            // 
+            // TXTName
+            // 
+            resources.ApplyResources(this.TXTName, "TXTName");
+            this.TXTName.Name = "TXTName";
+            // 
+            // LBLAge
+            // 
+            resources.ApplyResources(this.LBLAge, "LBLAge");
+            this.LBLAge.Name = "LBLAge";
+            // 
+            // TXTAge
+            // 
+            resources.ApplyResources(this.TXTAge, "TXTAge");
+            this.TXTAge.Name = "TXTAge";
+            // 
+            // BTNWelcome
+            // 
+            resources.ApplyResources(this.BTNWelcome, "BTNWelcome");
+            this.BTNWelcome.Name = "BTNWelcome";
+            this.BTNWelcome.UseVisualStyleBackColor = true;
+            this.BTNWelcome.Click += new System.EventHandler(this.BTNWelcome_Click);
+            // 
+            // BTNExit
+            // 
+            resources.ApplyResources(this.BTNExit, "BTNExit");
+            this.BTNExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BTNExit.Name = "BTNExit";
+            this.BTNExit.UseVisualStyleBackColor = true;
+            this.BTNExit.Click += new System.EventHandler(this.BTNExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.BTNWelcome;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BTNExit;
+            this.Controls.Add(this.BTNExit);
+            this.Controls.Add(this.BTNWelcome);
+            this.Controls.Add(this.TXTAge);
+            this.Controls.Add(this.LBLAge);
+            this.Controls.Add(this.TXTName);
+            this.Controls.Add(this.LBLName);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBLName;
+        private System.Windows.Forms.TextBox TXTName;
+        private System.Windows.Forms.Label LBLAge;
+        private System.Windows.Forms.TextBox TXTAge;
+        private System.Windows.Forms.Button BTNWelcome;
+        private System.Windows.Forms.Button BTNExit;
+    }
+}
+
---
+++ Asignment13/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asignment13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNWelcome_Click(object sender, EventArgs e)
+        {
+            //من فضلك يا يوسف اقرأ الكود بعناية حتى التعليقات علشان تفهم مقاصدي جيدا
+            try
+            {
+                if (string.IsNullOrEmpty(TXTName.Text) && string.IsNullOrEmpty(TXTAge.Text))
+                {
+                    MessageBox.Show("لا يمكن ترك أماكن الكتابة خالية");
+                }
+                else if (string.IsNullOrEmpty(TXTName.Text))
+                {
+                    MessageBox.Show("لا يمكن ترك مربع الاسم خاليا");
+                }
+                else if (string.IsNullOrEmpty(TXTAge.Text))
+                {
+                    MessageBox.Show("لا يمكن ترك مربع العمر خاليا");
+                }
+                else
+                {
+                    MessageBox.Show("مرحبا يا " + TXTName.Text.Trim() + "، " + "إن عمرك هو: " + int.Parse(TXTAge.Text.Trim())+"عاما، متعك الله بالصحة والعافية");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("من فضلك أدخل عمرك بالأرقام الصحيحة فقط");
+            }
+            //على فكرة كان ممكن أعمل الكود اللي جاي  من غير finally بس حبيت اختبر نفسي هل أنا فاهم finally واللا لا
+            finally
+            {
+                TXTName.Clear();
+                TXTAge.Clear();
+                TXTName.Focus();
+            }
+        }
+
+        private void BTNExit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TXTName.Text) || string.IsNullOrEmpty(TXTAge.Text))
+            {                
+                if (MessageBox.Show("هل أنت متأكد حقاً من رغبتك في الخروج؟", "خروج", MessageBoxButtons.YesNo, MessageBoxIcon.Stop, MessageBoxDefaultButton.Button2)==DialogResult.Yes)
+                {
+                    MessageBox.Show("شكرا لاستخدامك هذا البرنامج، جاري الخروج... مع السلامة");
+                    Application.Exit();
+                }
+                else
+                {
+                    TXTName.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("لا يمكن الخروج الآن، بسبب وجود بعض البيانات، من فضلك قم بمراجعتها ثم مسحها إذا أردت الخروج ثم أعد المحاولة");
+                TXTName.Focus();
+            }
+        }
+    }
+}
